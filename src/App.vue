1072b0aee7bd86a0d96b9298cba978a2d7a2c51f
qq9694526 <15638830111@139.com>
2017-05-16 11:20:26 +0800

    first blood

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="app">
+		<iphone></iphone>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+	import iphone from './components/iphone/iphone'
+	export default {
+		name: 'app',
+		components: {
+			iphone
+		}
+	}
+</script>
+
+<style lang="less">
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+		img {
+			border-radius: 50%;
+		}
+	}
+</style>
